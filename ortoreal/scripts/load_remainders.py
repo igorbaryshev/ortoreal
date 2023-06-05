ff34556cbc55f6f0fd453b9a175649a70345f743
Igor Baryshev <igoralebar@gmail.com>
2023-06-05 04:31:46 +0300

    исправить скрипт добавления остатков и прихода

--- ortoreal/scripts/load_remainders.py
+++ ortoreal/scripts/load_remainders.py
@@ -9,11 +9,14 @@ def run():
         next(reader)
 
         batch_create = []
+        Item.objects.all().delete()
+
         for row in reader:
-            part = Part.objects.get(vendor_code=row[0])
+            try:
+                part = Part.objects.get(vendor_code=row[0])
+            except Part.DoesNotExist:
+                print(row[0])
             quantity = int(row[1])
-            batch_create += [
-                Item(part=part, warehouse=Item.Warehouse.S1)
-            ] * quantity
+            batch_create += [Item(part=part, arrived=True)] * quantity
 
         Item.objects.bulk_create(batch_create)
